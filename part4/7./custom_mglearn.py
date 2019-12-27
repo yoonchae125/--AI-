2ee21cfbc50d0829f5f0bf23a53e0ef5109103be
chaeyoon <yoonchae125@naver.com>
2019-12-27 13:17:12 +0900

    add all

---
+++ part4/7./custom_mglearn.py
@@ -0,0 +1,128 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap, colorConverter, LinearSegmentedColormap
+from sklearn.datasets import make_blobs
+from sklearn.svm import SVC
+
+def plot_2d_separator(classifier, X, fill=False, ax=None, eps=None, alpha=1,
+                      cm=ListedColormap(['#0000aa', '#ff2020']),
+                      linewidth=None, threshold=None, linestyle="solid"):
+    # binary?
+    if eps is None:
+        eps = X.std() / 2.
+
+    if ax is None:
+        ax = plt.gca()
+
+    x_min, x_max = X[:, 0].min() - eps, X[:, 0].max() + eps
+    y_min, y_max = X[:, 1].min() - eps, X[:, 1].max() + eps
+    xx = np.linspace(x_min, x_max, 1000)
+    yy = np.linspace(y_min, y_max, 1000)
+
+    X1, X2 = np.meshgrid(xx, yy)
+    X_grid = np.c_[X1.ravel(), X2.ravel()]
+    try:
+        decision_values = classifier.decision_function(X_grid)
+        levels = [0] if threshold is None else [threshold]
+        fill_levels = [decision_values.min()] + levels + [decision_values.max()]
+    except AttributeError:
+        # no decision_function
+        decision_values = classifier.predict_proba(X_grid)[:, 1]
+        levels = [.5] if threshold is None else [threshold]
+        fill_levels = [0] + levels + [1]
+    if fill:
+        ax.contourf(X1, X2, decision_values.reshape(X1.shape),
+                    levels=fill_levels, alpha=alpha, cmap=cm)
+    else:
+        ax.contour(X1, X2, decision_values.reshape(X1.shape), levels=levels,
+                   colors="black", alpha=alpha, linewidths=linewidth,
+                   linestyles=linestyle, zorder=5)
+
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    ax.set_xticks(())
+    ax.set_yticks(())
+
+
+def discrete_scatter(x1, x2, y=None, markers=None, s=10, ax=None,
+                     labels=None, padding=.2, alpha=1, c=None, markeredgewidth=None):
+    if ax is None:
+        ax = plt.gca()
+
+    if y is None:
+        y = np.zeros(len(x1))
+
+    unique_y = np.unique(y)
+
+    if markers is None:
+        markers = ['o', '^', 'v', 'D', 's', '*', 'p', 'h', 'H', '8', '<', '>'] * 10
+
+    if len(markers) == 1:
+        markers = markers * len(unique_y)
+
+    if labels is None:
+        labels = unique_y
+
+    # lines in the matplotlib sense, not actual lines
+    lines = []
+
+    current_cycler = mpl.rcParams['axes.prop_cycle']
+
+    for i, (yy, cycle) in enumerate(zip(unique_y, current_cycler())):
+        mask = y == yy
+        # if c is none, use color cycle
+        if c is None:
+            color = cycle['color']
+        elif len(c) > 1:
+            color = c[i]
+        else:
+            color = c
+        # use light edge for dark markers
+        if np.mean(colorConverter.to_rgb(color)) < .4:
+            markeredgecolor = "grey"
+        else:
+            markeredgecolor = "black"
+
+        lines.append(ax.plot(x1[mask], x2[mask], markers[i], markersize=s,
+                             label=labels[i], alpha=alpha, c=color,
+                             markeredgewidth=markeredgewidth,
+                             markeredgecolor=markeredgecolor)[0])
+
+    if padding != 0:
+        pad1 = x1.std() * padding
+        pad2 = x2.std() * padding
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        ax.set_xlim(min(x1.min() - pad1, xlim[0]), max(x1.max() + pad1, xlim[1]))
+        ax.set_ylim(min(x2.min() - pad2, ylim[0]), max(x2.max() + pad2, ylim[1]))
+
+    return lines
+
+
+def make_handcrafted_dataset():
+    # a carefully hand-designed dataset lol
+    X, y = make_blobs(centers=2, random_state=4, n_samples=30)
+    y[np.array([7, 27])] = 0
+    mask = np.ones(len(X), dtype=np.bool)
+    mask[np.array([0, 1, 5, 26])] = 0
+    X, y = X[mask], y[mask]
+    return X, y
+
+
+def plot_svm(log_C, log_gamma, ax=None):
+    X, y = make_handcrafted_dataset()
+    C = 10. ** log_C
+    gamma = 10. ** log_gamma
+    svm = SVC(kernel='rbf', C=C, gamma=gamma).fit(X, y)
+    if ax is None:
+        ax = plt.gca()
+    plot_2d_separator(svm, X, ax=ax, eps=.5)
+    # plot data
+    discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    # plot support vectors
+    sv = svm.support_vectors_
+    # class labels of support vectors are given by the sign of the dual coefficients
+    sv_labels = svm.dual_coef_.ravel() > 0
+    discrete_scatter(sv[:, 0], sv[:, 1], sv_labels, s=15, markeredgewidth=3, ax=ax)
+    ax.set_title("C = %.4f gamma = %.4f" % (C, gamma))
